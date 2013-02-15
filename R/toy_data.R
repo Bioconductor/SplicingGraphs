e37a0007911220c09fec37ccfb57edfa21745c3c
Herve Pages <hpages@fhcrc.org>
2013-02-15 23:23:33 +0000

    Add plotToyReads(), a little helper for plotting toy reads on top of toy genes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SplicingGraphs@73568 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/toy_data.R
+++ R/toy_data.R
@@ -32,3 +32,38 @@ toy_reads_bam <- function()
     toy_reads_bam
 }
 
+### Displaying bug in Gviz (reported to Florian on Feb. 15, 2013). The first
+### range has width 5 and therefore should cover 5 letters (including the 1st
+### T). The last range has width 1 (not 0) and should be visible.
+if (FALSE) {
+  library(Biostrings)
+  seq <- DNAStringSet(c(chrX="ACCGACTTCA"))
+  library(GenomicRanges)
+  gr <- GRanges("chrX", IRanges(3, width=5:1))
+  library(Gviz) 
+  plotTracks(list(GenomeAxisTrack(), SequenceTrack(seq), AnnotationTrack(gr)),
+             from=1, to=9)
+}
+
+plotToyReads <- function(gal, txdb, from=NULL, to=NULL)
+{
+    ax_track <- GenomeAxisTrack()
+    txdb_track <- GeneRegionTrack(txdb, name="genes")
+    grl <- grglist(gal)
+    gr <- unlist(grl)
+    mcols(gr)$group <- names(gr)
+    grl <- relist(unname(gr), grl)
+    name <- if (length(grl) == 1L) names(grl)[1L] else "reads"
+    gal_track <- AnnotationTrack(grl, name=name, fill="blue", shape="box")
+    if (is.null(from) || is.null(to)) {
+        gal_min_start <- min(start(gal))
+        gal_max_end <- max(end(gal))
+        margin <- 0.10 * (gal_max_end - gal_min_start)
+        if (is.null(from))
+            from <- gal_min_start - margin
+        if (is.null(to))
+            to <- gal_max_end + margin
+    }
+    plotTracks(list(ax_track, gal_track, txdb_track), from=from, to=to)
+}
+
