7341eb9aa4aef07ecfadc101cc02054b1ee94a0c
Herve Pages <hpages@fhcrc.org>
2013-03-27 09:15:33 +0000

    More improvements to sgedgesByTranscript().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SplicingGraphs@74916 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sgedgesByTranscript-methods.R
+++ R/sgedgesByTranscript-methods.R
@@ -32,8 +32,11 @@ setMethod("sgedgesByTranscript", "SplicingGraphs",
         ## Add missing metadata cols to 'in_unlistData'.
         ex_unlistData <- ex_by_tx@unlistData
         ex_unlistData_len <- length(ex_unlistData)
+        ex_unlistData_mcols <- mcols(ex_unlistData)
         in_unlistData <- in_by_tx@unlistData
         in_unlistData_len <- length(in_unlistData)
+        in_unlistData_mcols <- mcols(in_unlistData)
+
         in_missing_mcols <- DataFrame(exon_id=NA_integer_,
                                       exon_name=NA_character_,
                                       exon_rank=NA_integer_,
@@ -41,35 +44,38 @@ setMethod("sgedgesByTranscript", "SplicingGraphs",
                                       end_SSid=NA_integer_)
         idx <- rep.int(1L, in_unlistData_len)
         in_missing_mcols <- in_missing_mcols[idx, , drop=FALSE]
-        mcols(in_unlistData) <- cbind(in_missing_mcols, mcols(in_unlistData))
+        in_unlistData_mcols <- cbind(in_missing_mcols, in_unlistData_mcols)
 
-        ## Add "from" and "to" metadata cols to 'ex_unlistData'
-        ## and 'in_unlistData'.
-        ex_unlistData_mcols <- mcols(ex_unlistData)
+        ## Make "from" and "to" metadata cols.
         from <- as.character(ex_unlistData_mcols$start_SSid)
         to <- as.character(ex_unlistData_mcols$end_SSid)
         idx <- which(strand(ex_unlistData) == "-")
         tmp <- from[idx]
         from[idx] <- to[idx]
         to[idx] <- tmp
-        mcols(ex_unlistData)$from <- from
-        mcols(ex_unlistData)$to <- to
+        ex_prepend_mcols <- DataFrame(from=from, to=to)
 
         from <- to <- rep.int(NA_character_, in_unlistData_len)
-        mcols(in_unlistData)$from <- from
-        mcols(in_unlistData)$to <- to
+        in_prepend_mcols <- DataFrame(from=from, to=to)
 
-        ## Add "ex_or_in" and "tx_id" metadata cols to 'ex_unlistData'
-        ## and 'in_unlistData'.
-        mcols(ex_unlistData)$ex_or_in <- factor("ex", levels=EX_OR_IN_LEVELS)
-        mcols(in_unlistData)$ex_or_in <- factor("in", levels=EX_OR_IN_LEVELS)
+        ## Make "ex_or_in" and "tx_id" metadata cols.
+        ex_or_in <- rep.int(factor("ex", levels=EX_OR_IN_LEVELS),
+                            ex_unlistData_len)
+        ex_prepend_mcols$ex_or_in <- ex_or_in
+        ex_or_in <- rep.int(factor("in", levels=EX_OR_IN_LEVELS),
+                            in_unlistData_len)
+        in_prepend_mcols$ex_or_in <- ex_or_in
+
         tx_id <- mcols(ex_by_tx)[ , "tx_id"]
         tx_id <- factor(tx_id, levels=unique(tx_id))
-        mcols(ex_unlistData)$tx_id <- rep.int(tx_id, nex_by_tx)
-        mcols(in_unlistData)$tx_id <- rep.int(tx_id, nin_by_tx)
+        ex_prepend_mcols$tx_id <- rep.int(tx_id, nex_by_tx)
+        in_prepend_mcols$tx_id <- rep.int(tx_id, nin_by_tx)
+
+        mcols(ex_unlistData) <- cbind(ex_prepend_mcols, ex_unlistData_mcols)
+        mcols(in_unlistData) <- cbind(in_prepend_mcols, in_unlistData_mcols)
 
         ## Compute 'ans_unlistData'. We need to reorder 'c(ex_unlistData,
-        ## in_unlistData)' to bring introns between the corresponding exons.
+        ## in_unlistData)' to bring introns between their flanking exons.
         ans_unlistData <- c(ex_unlistData, in_unlistData)
         seq0 <- seq_along(ex_partitioning)
         roidx <- integer(ex_unlistData_len + in_unlistData_len)
@@ -88,10 +94,10 @@ setMethod("sgedgesByTranscript", "SplicingGraphs",
         introns_idx <- which(is.na(from))  # same as 'which(is.na(to))'
         from[introns_idx] <- to[introns_idx - 1L]
         to[introns_idx] <- from[introns_idx + 1L]
-        mcols(ans_unlistData)$from <- from
-        mcols(ans_unlistData)$to <- to
+        ans_unlistData_mcols$from <- from
+        ans_unlistData_mcols$to <- to
 
-        ## Sanity check: comparing intron starts/ends with exon starts/ends.
+        ## Sanity check: exons must be flanking introns.
         ans_unlistData_start <- start(ans_unlistData)
         ans_unlistData_end <- end(ans_unlistData)
         ans_unlistData_strand <- strand(ans_unlistData)
@@ -109,6 +115,13 @@ setMethod("sgedgesByTranscript", "SplicingGraphs",
                             ans_unlistData_end[minus_introns_idx + 1L]))
         stopifnot(identical(ans_unlistData_end[minus_introns_idx] + 1L,
                             ans_unlistData_start[minus_introns_idx - 1L]))
+
+        ## Add "sgedge_id" metadata col.
+        sgedge_id <- paste0(rep.int(gene_ids, width(ans_partitioning)), ":",
+                            from, ",", to)
+        ans_unlistData_mcols <- cbind(DataFrame(sgedge_id=sgedge_id),
+                                      ans_unlistData_mcols)
+        mcols(ans_unlistData) <- ans_unlistData_mcols
 
         ## Relist 'ans_unlistData' and return.
         ans <- relist(ans_unlistData, ans_partitioning)
