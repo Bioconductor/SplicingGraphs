05410e1089084b06078f393168af043a1850ee6c
Herve Pages <hpages@fhcrc.org>
2013-04-29 18:02:30 +0000

    Add getReads(). Similar to countReads() but returns right before the final counting step, that is, the returned DataFrame contains the reads instead of their counts.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SplicingGraphs@76127 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/countReads-methods.R
+++ R/countReads-methods.R
@@ -1,35 +1,37 @@
 ### =========================================================================
-### Summarizing the reads assigned to the edges of a SplicingGraphs object
+### Summarizing the reads assigned to a SplicingGraphs object
 ### -------------------------------------------------------------------------
 
 
-.countReads_by_sgedge <- function(x)
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### getReads()
+###
+
+.getReads_by_sgedge <- function(x)
 {
     edges_by_gene <- sgedgesByGene(x, with.hits.mcols=TRUE)
     edge_data <- mcols(unlist(edges_by_gene, use.names=FALSE))
     edge_data_colnames <- colnames(edge_data)
     hits_mcol_idx <- grep("\\.hits$", edge_data_colnames)
-    ans <- endoapply(edge_data[hits_mcol_idx], elementLengths)
-    colnames(ans) <- sub("\\.hits$", "", colnames(ans))
+    hits_cols <- edge_data[hits_mcol_idx]
     left_mcolnames <- c("sgedge_id", "ex_or_in")
     left_cols <- edge_data[left_mcolnames]
-    cbind(left_cols, ans)
+    cbind(left_cols, hits_cols)
 }
 
-.countReads_by_rsgedge <- function(x)
+.getReads_by_rsgedge <- function(x)
 {
     edges_by_gene <- rsgedgesByGene(x, with.hits.mcols=TRUE)
     edge_data <- mcols(unlist(edges_by_gene, use.names=FALSE))
     edge_data_colnames <- colnames(edge_data)
     hits_mcol_idx <- grep("\\.hits$", edge_data_colnames)
-    ans <- endoapply(edge_data[hits_mcol_idx], elementLengths)
-    colnames(ans) <- sub("\\.hits$", "", colnames(ans))
+    hits_cols <- edge_data[hits_mcol_idx]
     left_mcolnames <- c("rsgedge_id", "ex_or_in")
     left_cols <- edge_data[left_mcolnames]
-    cbind(left_cols, ans)
+    cbind(left_cols, hits_cols)
 }
 
-.countReads_by_tx <- function(x)
+.getReads_by_tx <- function(x)
 {
     ex_by_tx <- unlist(x)
     tx_id <- mcols(ex_by_tx)[ , "tx_id"]
@@ -44,20 +46,18 @@
     ## a function 'FUN' that modifies the nb of rows. Furthermore, the
     ## returned object passes validation despite being broken! Fix it
     ## in IRanges.
-    ans <- endoapply(edge_data[hits_mcol_idx],
-                     function(hits)
-                       elementLengths(unique(regroup(hits,
-                                                     edge_data_breakpoints))))
+    hits_cols <- endoapply(edge_data[hits_mcol_idx],
+                           function(hits)
+                             unique(regroup(hits, edge_data_breakpoints)))
 
     ## Fix the broken DataFrame returned by endoapply().
-    ans@nrows <- length(tx_id)
-    ans@rownames <- NULL
+    hits_cols@nrows <- length(tx_id)
+    hits_cols@rownames <- NULL
 
-    colnames(ans) <- sub("\\.hits$", "", colnames(ans))
-    cbind(DataFrame(tx_id=tx_id, gene_id=gene_id), ans)
+    cbind(DataFrame(tx_id=tx_id, gene_id=gene_id), hits_cols)
 }
 
-.countReads_by_gene <- function(x)
+.getReads_by_gene <- function(x)
 {
     edges_by_gene <- sgedgesByGene(x, with.hits.mcols=TRUE)
     edge_data <- mcols(unlist(edges_by_gene, use.names=FALSE))
@@ -69,20 +69,42 @@
     ## a function 'FUN' that modifies the nb of rows. Furthermore, the
     ## returned object passes validation despite being broken! Fix it
     ## in IRanges.
-    ans <- endoapply(edge_data[hits_mcol_idx],
+    hits_cols <- endoapply(edge_data[hits_mcol_idx],
                      function(hits)
-                       elementLengths(unique(regroup(hits,
-                                                     edge_data_breakpoints))))
+                       unique(regroup(hits, edge_data_breakpoints)))
 
     ## Fix the broken DataFrame returned by endoapply().
-    ans@nrows <- length(edges_by_gene)
-    ans@rownames <- NULL
+    hits_cols@nrows <- length(edges_by_gene)
+    hits_cols@rownames <- NULL
 
-    colnames(ans) <- sub("\\.hits$", "", colnames(ans))
     gene_id <- names(edges_by_gene)
     tx_id <- unique(regroup(edge_data[ , "tx_id"], edge_data_breakpoints))
-    cbind(DataFrame(gene_id=gene_id, tx_id=tx_id), ans)
+    cbind(DataFrame(gene_id=gene_id, tx_id=tx_id), hits_cols)
 }
+
+setGeneric("getReads", signature="x",
+    function(x, by=c("sgedge", "rsgedge", "tx", "gene"))
+        standardGeneric("getReads")
+)
+
+### Return a DataFrame with 1 row per splicing graph edge (or reduced
+### splicing graph edge), and 1 column per sample.
+setMethod("getReads", "SplicingGraphs",
+    function(x, by=c("sgedge", "rsgedge", "tx", "gene"))
+    {
+        by <- match.arg(by)
+        switch(by,
+            sgedge=.getReads_by_sgedge(x),
+            rsgedge=.getReads_by_rsgedge(x),
+            tx=.getReads_by_tx(x),
+            gene=.getReads_by_gene(x))
+    }
+)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### countReads()
+###
 
 setGeneric("countReads", signature="x",
     function(x, by=c("sgedge", "rsgedge", "tx", "gene"))
@@ -94,12 +116,13 @@ setGeneric("countReads", signature="x",
 setMethod("countReads", "SplicingGraphs",
     function(x, by=c("sgedge", "rsgedge", "tx", "gene"))
     {
-        by <- match.arg(by)
-        switch(by,
-            sgedge=.countReads_by_sgedge(x),
-            rsgedge=.countReads_by_rsgedge(x),
-            tx=.countReads_by_tx(x),
-            gene=.countReads_by_gene(x))
+        assigned_reads <- getReads(x, by=by)
+        hits_col_idx <- grep("\\.hits$", colnames(assigned_reads))
+        if (length(hits_col_idx) == 0L)
+            return(assigned_reads)
+        read_counts <- endoapply(assigned_reads[hits_col_idx], elementLengths)
+        colnames(read_counts) <- sub("\\.hits$", "", colnames(read_counts))
+        cbind(assigned_reads[-hits_col_idx], read_counts)
     }
 )
 
